68eaa1e79e15c3775a7dfdf83843be7163ae3c36
Vladimir Mikryukov <v.mikryukov@gmail.com>
2024-12-16 13:38:14 +0200

    add custom polygon logic

--- bin/subset_data.R
+++ bin/subset_data.R
@@ -90,3 +90,117 @@ BASISOFRECORD <- to_na( opt$basisofrecord )
 DATA <- opt$data
 
 
+## Initialize the grid-cells of interest
+HEXES <- vector(mode = "character")
+
+## Get grid-cells from a polygon
+if(! is.na(POLYGON)) {
+
+  cat("Loading user-supplied polygon\n")
+  polygon_ext <- tolower( tools::file_ext(POLYGON) )
+  
+  ## Supported extensions
+  ext_wkt     <- c("wkt", "wkb", "ewkt", "ewkb", "agf")
+  ext_gpkg    <- c("gpkg", "geopackage")
+  ext_geojson <- c("geojson", "json", "geojsonp", "geojsons", "geojsonsl", "geojsonss", "geojsonsz")
+
+  if(polygon_ext %in% ext_wkt){
+    cat("..based on file extension, WKT format detected\n")
+    
+    # POLY <- geos::geos_read_wkt(wkt = POLYGON, fix_structure = TRUE)
+    # POLY <- wk::read_wkt(POLY)
+    # cat("..WKT summary:\n")
+    # print(wk::wk_count(POLY))
+
+    # WKT <- geos::geos_write_wkt(POLY)   # fixed in case of errors
+
+    # https://github.com/cran/rgeos/blob/master/R/rgeos_wkt.R
+
+    cat("Currently, support for WKT is not implemented\n")
+    stop()
+  }
+
+  if(!polygon_ext %in% c(ext_gpkg, ext_geojson)){
+    cat("The specified polygon file has an unsupported extension.\n", file=stderr())
+    cat("Supported extensions (case-insensitive): ", paste(c(ext_gpkg, ext_geojson), collapse = ", "), "\n")
+    stop()
+  }
+
+  if(polygon_ext %in% c("gpkg", "geopackage")){
+    cat("..based on file extension, GeoPackage format detected\n")
+  }
+  if(polygon_ext %in% ext_geojson){
+    cat("..based on file extension, GeoJSON format detected\n")
+  }
+
+  ## Read the user-supplied polygon file
+  cat("...reading the polygon file\n")
+  POLY <- st_read(POLYGON, promote_to_multi = TRUE)
+
+  ## Check geometry types
+  geom_types <- unique(st_geometry_type(POLY))
+  if(! all(geom_types %in% c("POLYGON", "MULTIPOLYGON"))){
+    unsupported_geoms <- geom_types[ ! geom_types %in% c("POLYGON", "MULTIPOLYGON") ]
+    unsupported_geoms <- paste(unsupported_geoms, collapse = ", ")
+    cat("...provided polygon has unsupported geometry type: ", unsupported_geoms, "\n")
+    cat("...only POLYGON or MULTIPOLYGON are supported\n")
+    cat("...unsupported geometries will be ignored\n")
+    POLY <- POLY[ st_geometry_type(POLY) %in% c("POLYGON", "MULTIPOLYGON"), ]
+    cat("...number of geometries after filtering: ", nrow(POLY), "\n")
+    if(nrow(POLY) == 0){
+      cat("...no valid geometries left\n")
+      stop()
+    }
+  }
+
+  ## Check CRS
+  if(st_crs(POLY)$epsg != 4326){
+    cat("...provided polygon has CRS: EPSG ", st_crs(POLY)$epsg, "\n")
+    cat("...reprojecting to EPSG:4326\n")
+    POLY <- st_transform(POLY, crs = 4326)
+  }
+
+  ## Convert MULTIPOLYGONs to POLYGONs
+  cat("...converting MULTIPOLYGONs to POLYGONs\n")
+  POLY <- st_cast(x = POLY, to = "POLYGON", warn = FALSE, do_split = TRUE)
+
+  ## Convert to WKT format
+  cat("...converting to WKT format\n")
+  WKT <- st_as_text(POLY$geom)
+  
+  ## Get all hexagons with CENTERS contained in a given polygon
+  # POLYGON_HEXES <- h3::polyfill(POLY, res = RESOLUTION)
+
+  ## Export to file
+  cat("...exporting WKT to file\n")
+  tmp_wkt <- tempfile(pattern = "polygon_wkt", fileext = ".wkt")
+  fwrite(
+    x = data.table(Polygon = WKT),
+    file = tmp_wkt,
+    sep = "\t", quote = FALSE, row.names = FALSE, col.names = FALSE)
+
+  ## Get H3 cells that intersect with the polygon
+  cat("...getting H3 cells\n")
+  tmp_h3 <- tempfile(pattern = "polygon_h3", fileext = ".txt")
+  
+  system2(
+    command = system(command = "which wkt_polygon_to_h3_cells.sh", intern = TRUE),
+    args = c(
+      "-i", tmp_wkt, 
+      "-o", tmp_h3, 
+      "-r", RESOLUTION, 
+      "-a", "experimental", 
+      "-c", "CONTAINMENT_OVERLAPPING"),
+    stdout = "", stderr = "",             # output stdout and stderr to R console
+    wait = TRUE)
+
+  ## Load the H3 cells
+  POLYGON_HEXES <- fread(tmp_h3, header = TRUE, sep = "\t")
+  POLYGON_HEXES <- POLYGON_HEXES$h3_cell
+
+  ## Add to the main list of grid-cells
+  HEXES <- c(HEXES, POLYGON_HEXES)
+}
+
+
+
