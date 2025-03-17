1ec06518df6ab3af5dc6e162de0b492b1f4d1ed0
Vladimir Mikryukov <v.mikryukov@gmail.com>
2025-03-17 14:24:36 +0200

    upd hyp tests

--- bin/hypothesis_test.R
+++ bin/hypothesis_test.R
@@ -80,3 +80,91 @@ cat("  Phylogenetic tree:",   TREE, "\n")
 cat("  H3 resolution:",       RESOLUTION, "\n")
 cat("  Results prefix:",      RESULTS, "\n")
 
+##########################################################
+########################################################## Load data
+##########################################################
+
+## Function to load polygons
+load_polygons <- function(poly, separate = FALSE){
+  # separate - if TRUE, return a list of polygons
+  
+  if(!file.exists(poly)){
+    cat("ERROR: File with polygons not found: ", poly, "\n", file=stderr())
+    stop()
+  }
+  
+  res <- try( st_read(poly, quiet = TRUE) )
+  
+  if("try-error" %in% class(res)){
+    cat("ERROR: Failed to read polygons from the file: ", poly, "\n", file=stderr())
+    stop()
+  }
+  
+  ## Check geometry types
+  geom_types <- unique(st_geometry_type(res))
+  if(! all(geom_types %in% c("POLYGON", "MULTIPOLYGON"))){
+    unsupported_geoms <- geom_types[ ! geom_types %in% c("POLYGON", "MULTIPOLYGON") ]
+    unsupported_geoms <- paste(unsupported_geoms, collapse = ", ")
+    cat("....provided polygon has unsupported geometry type: ", unsupported_geoms, "\n")
+    cat("....only POLYGON or MULTIPOLYGON are supported\n")
+    cat("....unsupported geometries will be ignored\n")
+    res <- res[ st_geometry_type(res) %in% c("POLYGON", "MULTIPOLYGON"), ]
+    cat("....number of geometries after filtering: ", nrow(res), "\n")
+  }
+
+  if(nrow(res) == 0){
+    cat("ERROR: No polygons found in the file: ", poly, "\n", file=stderr())
+    stop()
+  } else {
+    cat("...Loaded ", nrow(res), " geometries\n")
+  }
+
+  ## Check CRS
+  if(st_crs(res)$epsg != 4326){
+    cat("...provided polygon(s) has CRS: EPSG ", st_crs(res)$epsg, "\n")
+    cat("...reprojecting to EPSG:4326\n")
+    res <- st_transform(res, crs = 4326)
+  }
+
+  ## TODO: --- split geometries if requested --> if multipolygons, split them independently within each geometry
+
+  ## Convert MULTIPOLYGONs to POLYGONs
+  if("MULTIPOLYGON" %in% st_geometry_type(res)){
+    cat("...converting MULTIPOLYGONs to POLYGONs\n")
+    res <- st_cast(x = res, to = "POLYGON", warn = FALSE, do_split = TRUE)
+  }
+
+  return(res)
+} # end of `load_polygons`
+
+
+cat("\n\n-------- Loading data --------\n\n")
+
+
+## Load species occurrences
+cat("..Loading species occurrences\n")
+occ <- open_dataset(OCCURRENCES)
+
+## Validate H3 resolution
+tmp <- occ %>% head(1) %>% collect()
+if(h3_get_resolution(tmp$H3) != RESOLUTION){
+  cat("ERROR: H3 resolution in the species occurrences does not match the specified resolution.\n", file=stderr())
+  stop()
+}
+rm(tmp)
+
+## Load polygons
+cat("..Loading reference polygons\n")
+polygons_reference <- load_polygons(POLYGONS_REFERENCE)
+
+cat("..Loading test polygons\n")
+polygons_test <- load_polygons(POLYGONS_TEST)
+
+
+## Load phylogenetic tree
+cat("..Loading phylogenetic tree\n")
+# tree <- ape::read.tree(TREE)               # Newick format
+tree <- ape::read.nexus(file = TREE)         # Nexus format
+
+
+
